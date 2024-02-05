dc81f9455737e5583861a678ee4644b021aafad0
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-05 00:31:21 -0600

    Course y derivados realizado (model, router, controllers)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routerStudent = require('./student.router');
+const routerCourse = require('./course.router');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use("/students", routerStudent);
+router.use("/courses", routerCourse);
 
 
 module.exports = router;
