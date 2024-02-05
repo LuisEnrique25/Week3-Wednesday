3d7f175aa2e7b3edc56517a068dd6fd1d182b9d0
LuisEnrique25 <luisrg2225@gmail.com>
2024-02-05 00:22:18 -0600

    Derivados de Student realizados (model, controllers, router)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const routerStudent = require('./student.router');
 const router = express.Router();
 
 // colocar las rutas aquí
+router.use("/students", routerStudent);
 
 
 module.exports = router;
